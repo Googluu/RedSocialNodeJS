4ffbfacd1c4a785727b72668552e8b5b1662d8e9
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-06-20 22:55:11 -0500

    Creando la estructura principal

--- api/index.js
+++ api/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const swaggerUi = require('swagger-ui-express');
 
 const {config} = require('../config');
 const user = require('./routes/users.router');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+const swaggerDoc = require('./swagger.json');
+
 // Router
 app.use('/api/user', user);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.port, () => {
     console.log('Api escuchando en el puerto ', config.port);
