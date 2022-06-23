e32a87610e31c1f574381219aa4ae3e5b650ee5a
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-06-22 23:20:27 -0500

    Puesta en producción serverless

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    remoteDB: process.env.REMOTE_DB || false,
     api: {
         port: process.env.API_PORT || 3000,
     },
@@ -9,10 +10,10 @@ module.exports = {
         secret: process.env.JWT_SECRET || 'notasecret!',
     },
     mysql: {
-        host: process.env.MYSQL_HOST || 'remotemysql.com',
-        user: process.env.MYSQL_USER || 'hyUZi6F9bE',
-        password: process.env.MYSQL_PASS || '2eK06nVZUw',
-        database: process.env.MYSQL_DB || 'hyUZi6F9bE',
+        host: process.env.MYSQL_HOST || '',
+        user: process.env.MYSQL_USER || '',
+        password: process.env.MYSQL_PASS || '',
+        database: process.env.MYSQL_DB || '',
     },
     mysqlService: {
         host: process.env.MYSQL_SRV_HOST || 'localhost',
