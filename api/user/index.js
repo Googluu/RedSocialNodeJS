ce3d827f7de37e8fcc26e37350d9f3e6355c10de
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-06-23 07:13:55 -0500

    Cacheando nuestra aplicación

--- api/user/index.js
+++ api/user/index.js
@@ -2,11 +2,14 @@
 const config = require('../../config');
 
 let store;
+let cache;
 if (config.remoteDB === true) {
     store = require('../../store/remote.mysql');
+    cache = require('../../store/remote.cache');
 } else {
     store = require('../../store/mysql');
+    cache = require('../../store/redis');
 }
 const ctrl = require('./controller');
 
-module.exports = ctrl(store);
+module.exports = ctrl(store, cache);
